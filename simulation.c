40ad4336d146856166ab9ec62587d297c31f3f18
Unknown <iomaganaris@gmail.com>
2018-02-27 12:29:45 +0200

    initialization variables

--- simulation.c
+++ simulation.c
@@ -5,9 +5,26 @@
 
 int main(void){
 
-	printf("helloworld");
+	int nruns = 1;
+
+	for(int nrun = 0; i < nruns; nrun){
+		double realtime = 0;
+		double stime = 1; //second
+		double stime2 = 50; //second
+
+		double resolution_export = 10; //every x ms
+
+		int N = 10;
+		double taum = 10; //ms
+		double Ee = 0; //mV
+		double tuae = 2; //ms
+		double Fon = 50; //Hz
+		double Foff = 3; //Hz
+
+		double s = 100*
 
 
 
-
+	}
+	return 0;
 }
